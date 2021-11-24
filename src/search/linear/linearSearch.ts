f046fd5538121e06521a303e1861e144f5c7d5ae
boon-cpu <harrisonboon@gmail.com>
2021-11-24 10:09:10 +0000

    Linear Search :white_check_mark:

--- src/search/linear/linearSearch.ts
+++ src/search/linear/linearSearch.ts
@@ -1,8 +1,16 @@
 import { NO_MATCH } from "../common";
 
-const linearSearch = (items: any[], item: any): number => {
+const linearSearch = (
+  items: string[] | number[],
+  item: string | number
+): number => {
+  for (let i = 0; i < items.length; i++) {
+    if (items[i] == item) {
+      return i;
+    }
+  }
 
-    return NO_MATCH;
-}
+  return NO_MATCH;
+};
 
 export default linearSearch;
