5f7fe6048fed57b734e257b9dc1a82baceacdea5
boon-cpu <harrisonboon@gmail.com>
2021-11-24 22:37:44 +0000

    Merge Sort + Testing :white_check_mark:

---
+++ src/sort/mergeSort/mergeSort.ts
@@ -0,0 +1,32 @@
+function mergeSort<T>(array: T[]): T[] {
+  if (!array.length) return array;
+
+  return mergeSortRecursive(array, 0, array.length - 1);
+}
+
+function mergeSortRecursive<T>(
+  array: T[],
+  lPointer: number,
+  rPointer: number
+): T[] {
+  if (lPointer == rPointer) return [array[lPointer]];
+
+  const middle = ~~((lPointer + rPointer) / 2);
+
+  const firstHalf = mergeSortRecursive(array, lPointer, middle);
+  const secondHalf = mergeSortRecursive(array, middle + 1, rPointer);
+
+  array = [];
+
+  while (firstHalf.length > 0 && secondHalf.length > 0) {
+    if (firstHalf[0] > secondHalf[0]) array.push(secondHalf.shift()!);
+    else array.push(firstHalf.shift()!);
+  }
+
+  array.push(...firstHalf);
+  array.push(...secondHalf);
+
+  return array;
+}
+
+export default mergeSort;
