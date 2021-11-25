0f8e62a73872bcb46d9f277c5fdc85dd2880ada8
boon-cpu <60711166+boon-cpu@users.noreply.github.com>
2021-11-25 09:35:08 +0000

    The
    Here's my code

--- src/search/linear/linearSearch.ts
+++ src/search/linear/linearSearch.ts
@@ -1,16 +1,13 @@
 import { NO_MATCH } from "../common";
 
-const linearSearch = (items: any[], item: any): number => {
-    console.log('Foo')
-    for(let index=0; index<items.length;index++) {
-        console.log(`Comapring ${items[index]} to ${item}`)
-        if(items[index] === item) {
-            console.log(`MATCH FOUND AT ${index}`)
-            return index;
-        }
+function linearSearch<T>(items: T[], item: T): number {
+  for (let i = 0; i < items.length; i++) {
+    if (items[i] == item) {
+      return i;
     }
+  }
 
-    return NO_MATCH;
+  return NO_MATCH;
 }
 
 export default linearSearch;
